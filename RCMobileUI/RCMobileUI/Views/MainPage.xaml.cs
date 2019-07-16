a76f6d85a585e9d9de9173ba451ad3e8806e693f
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-07-16 22:26:32 +0300

    Added projects to solution.
    Added DesktopUI project.
    Added Xamarin Forms project (Android / iOS).

---
+++ RCMobileUI/RCMobileUI/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RCMobileUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
