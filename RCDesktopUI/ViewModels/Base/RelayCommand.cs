658fd6a570db2568ac4814c78d01d8dfa310db64
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-07-17 16:13:05 +0300

    Add RelayCommand.

---
+++ RCDesktopUI/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RCDesktopUI.ViewModels.Base
+{
+    public class RelayCommand : ICommand
+    {
+        #region EventHandler
+
+        /// <summary>
+        /// Event called when CanExecute changes
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        private Action commandAction;
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="action">The action to be executed</param>
+        public RelayCommand(Action action)
+        {
+            this.commandAction = action;
+        }
+
+        #endregion
+
+        #region CanExecute And Execute Methods
+
+        /// <summary>
+        /// Always execute
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Execute Command
+        /// </summary>
+        public void Execute(object parameter)
+        {
+            this.commandAction();
+        }
+
+        #endregion
+    }
+}
