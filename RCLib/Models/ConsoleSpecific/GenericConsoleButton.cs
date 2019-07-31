94610e11cf54e9d8931968df909e1e2db39c0647
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-07-31 16:05:14 +0300

    Add default constructor and documentation to GenericConsoleButton.

--- RCLib/Models/ConsoleSpecific/GenericConsoleButton.cs
+++ RCLib/Models/ConsoleSpecific/GenericConsoleButton.cs
@@ -5,8 +5,37 @@
     /// </summary>
     public class GenericConsoleButton : IConsoleButton
     {
-        public ConsoleNameEnum ConsoleName { get; set; } = ConsoleNameEnum.NONE;
-        public GenericKeyNameEnum KeyName { get; set; } = GenericKeyNameEnum.NONE;
-        public ConsoleKeyStateEnum KeyState { get; set; } = ConsoleKeyStateEnum.NONE;
+        #region Public properties
+
+        /// <summary>
+        /// The name of the console this button is from
+        /// </summary>
+        public ConsoleNameEnum ConsoleName { get; set; }
+
+        /// <summary>
+        /// The name of this button
+        /// </summary>
+        public GenericKeyNameEnum KeyName { get; set; }
+
+        /// <summary>
+        /// The state of this button
+        /// </summary>
+        public ConsoleKeyStateEnum KeyState { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GenericConsoleButton()
+        {
+            this.ConsoleName = ConsoleNameEnum.NONE;
+            this.KeyName = GenericKeyNameEnum.NONE;
+            this.KeyState = ConsoleKeyStateEnum.NONE;
+        }
+
+        #endregion
     }
 }
