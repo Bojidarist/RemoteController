1c2b3b4017246c584c153651ce889d0bbe4aac9f
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-06 21:15:54 +0300

    Add KeyboardKeysEnum to RCLib.

---
+++ RCLib/Models/Enums/KeyboardKeysEnum.cs
@@ -0,0 +1,138 @@
+﻿namespace RCLib.Models.Enums
+{
+    /// <summary>
+    /// The keyboard keys
+    /// </summary>
+    public enum KeyboardKeysEnum
+    {
+        /// <summary>
+        /// The A key
+        /// </summary>
+        A = 0,
+
+        /// <summary>
+        /// The B key
+        /// </summary>
+        B = 1,
+
+        /// <summary>
+        /// The C key
+        /// </summary>
+        C = 2,
+
+        /// <summary>
+        /// The D key
+        /// </summary>
+        D = 3,
+
+        /// <summary>
+        /// The E key
+        /// </summary>
+        E = 4,
+
+        /// <summary>
+        /// The F key
+        /// </summary>
+        F = 5,
+
+        /// <summary>
+        /// The G key
+        /// </summary>
+        G = 6,
+
+        /// <summary>
+        /// The H key
+        /// </summary>
+        H = 7,
+
+        /// <summary>
+        /// The I key
+        /// </summary>
+        I = 8,
+
+        /// <summary>
+        /// The J key
+        /// </summary>
+        J = 9,
+
+        /// <summary>
+        /// The K key
+        /// </summary>
+        K = 10,
+
+        /// <summary>
+        /// The L key
+        /// </summary>
+        L = 11,
+
+        /// <summary>
+        /// The M key
+        /// </summary>
+        M = 12,
+
+        /// <summary>
+        /// The N key
+        /// </summary>
+        N = 13,
+
+        /// <summary>
+        /// The O key
+        /// </summary>
+        O = 14,
+
+        /// <summary>
+        /// The P key
+        /// </summary>
+        P = 15,
+
+        /// <summary>
+        /// The Q key
+        /// </summary>
+        Q = 16,
+
+        /// <summary>
+        /// The R key
+        /// </summary>
+        R = 17,
+
+        /// <summary>
+        /// The S key
+        /// </summary>
+        S = 18,
+
+        /// <summary>
+        /// The T key
+        /// </summary>
+        T = 19,
+
+        /// <summary>
+        /// The U key
+        /// </summary>
+        U = 20,
+
+        /// <summary>
+        /// The V key
+        /// </summary>
+        V = 21,
+
+        /// <summary>
+        /// The W key
+        /// </summary>
+        W = 22,
+
+        /// <summary>
+        /// The X key
+        /// </summary>
+        X = 23,
+
+        /// <summary>
+        /// The Y key
+        /// </summary>
+        Y = 24,
+
+        /// <summary>
+        /// The Z key
+        /// </summary>
+        Z = 25
+    }
+}
