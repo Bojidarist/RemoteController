e56aa1797bd4d265905203bc01a1e7a2c1ae3bcd
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-06 19:56:38 +0300

    Add SelectedNESKeys for storing the current selected keys.

---
+++ RCDesktopUI/SelectedControllerKeys/SelectedNESKeys.cs
@@ -0,0 +1,50 @@
+﻿using AutoSharp;
+
+namespace RCDesktopUI.SelectedControllerKeys
+{
+    /// <summary>
+    /// The selected keys for the NES console
+    /// </summary>
+    public static class SelectedNESKeys
+    {
+        /// <summary>
+        /// The A button
+        /// </summary>
+        public static KeyboardKeyCodes A = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The B button
+        /// </summary>
+        public static KeyboardKeyCodes B = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Start button
+        /// </summary>
+        public static KeyboardKeyCodes START = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Select button
+        /// </summary>
+        public static KeyboardKeyCodes SELECT = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Left button on the joypad
+        /// </summary>
+        public static KeyboardKeyCodes LEFTARROW = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Right button on the joypad
+        /// </summary>
+        public static KeyboardKeyCodes RIGHTARROW = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Up button on the joypad
+        /// </summary>
+        public static KeyboardKeyCodes UPARROW = KeyboardKeyCodes.VK__none_;
+
+        /// <summary>
+        /// The Down button on the joypad
+        /// </summary>
+        public static KeyboardKeyCodes DOWNARROW = KeyboardKeyCodes.VK__none_;
+    }
+}
