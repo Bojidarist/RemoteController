014778f394e151a86506201e7c98ebce0c8e876c
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-18 22:58:12 +0300

    Single instance check on startup in RCDesktopUI.

--- RCDesktopUI/App.xaml.cs
+++ RCDesktopUI/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Threading;
+using System;
 
 namespace RCDesktopUI
 {
@@ -7,5 +9,27 @@ namespace RCDesktopUI
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+
+        /// <summary>
+        /// This is executed when the process starts
+        /// </summary>
+        /// <param name="e">Startup arguments</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get the name of the current app
+            string appName = AppDomain.CurrentDomain.FriendlyName;
+            bool isNew;
+
+            _mutex = new Mutex(true, appName, out isNew);
+
+            if (!isNew)
+            {
+                // If the app is already running then exit
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
