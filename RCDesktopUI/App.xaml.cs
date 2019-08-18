6eec2a1febd6e6cd3fe6c95805a664b2b1fbaaef
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-19 00:37:23 +0300

    When exception is thrown log it in a Log file in RCDesktopUI.

--- RCDesktopUI/App.xaml.cs
+++ RCDesktopUI/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using System.Threading;
 using System;
+using System.IO;
+using System.Runtime.ExceptionServices;
 
 namespace RCDesktopUI
 {
@@ -17,6 +19,20 @@ namespace RCDesktopUI
         /// <param name="e">Startup arguments</param>
         protected override void OnStartup(StartupEventArgs e)
         {
+            // Close this instance another instance of the app is running
+            SingleInstaceCheck();
+
+            // Log all exceptions
+            AppDomain.CurrentDomain.FirstChanceException += FirstChanceExceptionHandler;
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Close this instance another instance of the app is running
+        /// </summary>
+        private void SingleInstaceCheck()
+        {
             // Get the name of the current app
             string appName = AppDomain.CurrentDomain.FriendlyName;
             bool isNew;
@@ -28,8 +44,27 @@ namespace RCDesktopUI
                 // If the app is already running then exit
                 Application.Current.Shutdown();
             }
+        }
 
-            base.OnStartup(e);
+        /// <summary>
+        /// Log exceptions when they are thrown
+        /// </summary>
+        private void FirstChanceExceptionHandler(object sender, FirstChanceExceptionEventArgs e)
+        {
+            bool fileExists = File.Exists("Log.txt");
+            using (StreamWriter writer = new StreamWriter("Log.txt", true))
+            {
+                if (fileExists)
+                {
+                    writer.WriteLine();
+                }
+                writer.WriteLine($"(UTC){ DateTime.UtcNow }: { e.Exception.GetType() } From { e.Exception.Source }");
+                writer.WriteLine($"Message: { e.Exception.Message }");
+                writer.WriteLine($"StackTrace: { e.Exception.StackTrace }");
+                writer.WriteLine($"Data: { e.Exception.Data }");
+                writer.WriteLine($"HelpLink: { e.Exception.HelpLink }");
+                writer.WriteLine($"HResult: { e.Exception.HResult }");
+            }
         }
     }
 }
