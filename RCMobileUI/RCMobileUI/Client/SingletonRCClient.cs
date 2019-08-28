b8a4cbedd7b685925f0af9702d34953778617a5b
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-28 21:45:59 +0300

    Add SingletonRCClient to RCMobileUI.

---
+++ RCMobileUI/RCMobileUI/Client/SingletonRCClient.cs
@@ -0,0 +1,15 @@
+﻿using RCLib.Client;
+
+namespace RCMobileUI.Client
+{
+    /// <summary>
+    /// A singleton <see cref="RCClient"/>
+    /// </summary>
+    static class SingletonRCClient
+    {
+        /// <summary>
+        /// The client
+        /// </summary>
+        public static RCClient SingleRCClient { get; } = new RCClient();
+    }
+}
