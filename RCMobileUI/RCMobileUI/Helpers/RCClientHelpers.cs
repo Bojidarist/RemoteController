3c3d478ecc66d0a7e7df4f475d5e87baeb62f3c0
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-08-29 20:07:36 +0300

    Add Helpers for RCClient in RCMobileUI.

---
+++ RCMobileUI/RCMobileUI/Helpers/RCClientHelpers.cs
@@ -0,0 +1,61 @@
+﻿using RCLib.Client;
+using SimpleTCPStandar;
+using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace RCMobileUI.Helpers
+{
+    /// <summary>
+    /// Methods for easier use of <see cref="RCClient"/> in <see cref="RCMobileUI"/>
+    /// </summary>
+    public static class RCClientHelpers
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        /// <summary>
+        /// Pings to see if a RemoteController server is active and if it is, return the IP
+        /// </summary>
+        /// <param name="client">The <see cref="RCClient"/> object</param>
+        public static string GetActiveServerIP(this RCClient client)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            int maxLength = 254;
+            string localIP = new SimpleTcpServer().GetIPAddresses().FirstOrDefault().MapToIPv4().ToString();
+            if (localIP == null)
+            {
+                return null;
+            }
+
+            localIP = localIP.Remove(localIP.LastIndexOf('.'));
+            string localIPWithoutID = localIP;
+            for (int i = 0; i <= maxLength; i++)
+            {
+                try
+                {
+                    localIP = $"{ localIPWithoutID }.{ i }";
+                    using (var tcpClient = new TcpClient())
+                    {
+                        if (!tcpClient.ConnectAsync(localIP, 8910).Wait(100))
+                        {
+                            throw new Exception();
+                        }
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    if (i == maxLength)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            return localIP;
+        }
+    }
+}
